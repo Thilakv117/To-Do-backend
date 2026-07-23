984a4d41401571a9e456c3d9ad24b65130924a5e
Thilak <thilakv117@gmail.com>
2026-07-23 23:35:30 +0530

    Added a post method

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from typing import Annotated
-
 from fastapi import Depends, FastAPI
+from pydantic import BaseModel
 import model
 from database import engine, sessionLocal
 from sqlalchemy.orm import Session
@@ -24,4 +24,39 @@ def get_db():
 # for dependency injection
 
 db_dependency = Annotated[Session, Depends(get_db)]
+
+# creating a class for task
+
+
+
+class TaskCreate(BaseModel):
+    title: str
+    description: str
+    is_completed: bool
+    
+class TaskResponse(BaseModel):
+    id: int
+    title: str
+    description: str
+    is_completed: bool
+    
+@app.post("/task")
+def create_user(addTask: TaskCreate, db: db_dependency):
+    try:
+        db_task = model.Test(
+            title = addTask.title,
+            description=addTask.description,
+            is_completed= addTask.is_completed
+        )
+        db.add(db_task)
+        db.commit()
+        db.refresh(db_task)
+        response = {
+            "Data": [db_task]
+        }
+        return response
+    except Exception as e:
+        return str(e)
+
+    
 
--- model.py
+++ model.py
@@ -1,9 +1,12 @@
 from sqlalchemy import Boolean, String, Column, ForeignKey, Integer
 from database import BASE
 
+
 class Test(BASE):
-    __tablename__ = 'test'
+    __tablename__ = 'task'
     id = Column(Integer, primary_key=True, index=True)
-    task = Column(String, index=True)
+    title = Column(String, index=True)
+    description = Column(String, index=True)
+    is_completed = Column(Boolean, default=False, index=True)
     
     
