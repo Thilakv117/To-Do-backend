275d6e54be82f8e42fc20e0294fa0e6a38af6dc7
Thilak <thilakv117@gmail.com>
2026-07-26 01:11:17 +0530

    Added post and get API

--- main.py
+++ main.py
@@ -30,9 +30,9 @@ db_dependency = Annotated[Session, Depends(get_db)]
 
 
 class TaskCreate(BaseModel):
-    title: str
-    description: str
-    is_completed: bool
+    title: str | None = None
+    description: str | None = None
+    is_completed: bool | None = None
     
 class TaskResponse(BaseModel):
     id: int
@@ -68,6 +68,48 @@ def task_delete(id:int, db: db_dependency):
         return "Deleted successfuly"
     except Exception as e:
         return {"eMessage": str(e)}
+    
+@app.get("/task")
+def get_task(db: db_dependency):
+    try:
+        tasks = model.Test
+        users = db.query(tasks).all()
+        return users
+    except Exception as e:
+        return {
+            "eMessage": str(e)
+        }
+        
+@app.patch("/task/{id}")
+def update_task(id: int, updateTask: TaskCreate, db: db_dependency):
+    try:
+        filter_task = db.query(model.Test).filter(model.Test.id == id).first()
+        if filter_task is None:
+            return {
+                "eMesage": f"{id} not found"
+            }
+        update_data = updateTask.model_dump(exclude_unset=True)
+        update_data = updateTask.model_dump(exclude_unset=True)
+        
+        for key, value in update_data.items():
+            setattr(filter_task, key, value)
+        db.commit()
+        db.refresh(filter_task)
+    
+        return {
+            "data": filter_task
+        }
+    except Exception as e:
+        return {"eMessage": e}
+    
+    
+                
+    
+    
+    
+
+
+    
 
     
 
